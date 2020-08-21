d8828dc7b072bbb63ac404f0e5b1fecf45edee69
wenli <55821268+stevenreally6@users.noreply.github.com>
2020-08-22 04:49:34 +0800

    Add files via upload
    the files are MATLAB 2019

---
+++ ARM robot planning/Jacobian_inverse.m
@@ -0,0 +1,35 @@
+function output=Jacobian_inverse(input)
+%INPUT is (theta1, theta2, d3, theta4,xd_dot_plus_Ke) 
+theta1=input(5,:); d1=0; a1=0.5; alpha1=0; %parameter link1
+theta2=input(6,:); d2=0; a2=0.5; alpha2=0; %parameter link2
+theta3=0; d3=input(7,:); a3=0; alpha3=0;  %parameter link3
+theta4=input(8,:); d4=0; a4=0; alpha4=0;  %parameter link4
+ T01=DHmatrix(theta1,d1,a1,alpha1);
+ T12=DHmatrix(theta2,d2,a2,alpha2);
+ T23=DHmatrix(theta3,d3,a3,alpha3);
+ T34=DHmatrix(theta4,d4,a4,alpha4);
+ T02=T01*T12;
+ T03=T01*T12*T23;
+ T04=T01*T12*T23*T34;
+P0=[0;0;0];
+P1=T01(1:3,4);
+P3=T03(1:3,4);
+P4=T04(1:3,4);
+
+Z0=[0;0;1];
+Jv1=cross(Z0,(P4-P0));
+Jv2=cross(Z0,(P4-P1));
+Jv3=Z0;
+Jv4=cross(Z0,(P4-P3));
+Jv=[Jv1 Jv2 Jv3 Jv4;1 1 0 1];
+output=inv(Jv)*input(1:4,:);
+end
+
+function [Mdh] = DHmatrix(theta,d,a,alpha)
+%DHMATRIX Summary of this function goes here
+%   inputannya DH parameter
+Mdh=[cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta);
+     sin(theta) cos(theta)*cos(alpha)  -cos(theta)*sin(alpha) a*sin(theta);
+     0,sin(alpha),cos(alpha),d;
+     0,0,0,1];
+end
